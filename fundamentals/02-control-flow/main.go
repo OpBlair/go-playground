b1390b632dbb15bbbc98b44ec43ac2b84cff87d9
Tonny Blair <jusanotherthursday@gmail.com>
2026-07-28 13:04:11 +0300

    Update main.go

--- fundamentals/02-control-flow/main.go
+++ fundamentals/02-control-flow/main.go
@@ -1,6 +1,8 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
 	fmt.Println("Control Flow")
@@ -25,5 +27,98 @@ func main() {
 	fmt.Println("\nCounting up:")
 	for i := 1; i <= 3; i++ {
 		fmt.Printf("Count: %d\n", i)
+	}
+
+	FizzBuzz()
+	LeapYearValidator()
+	Calculator()
+}
+
+func FizzBuzz() {
+	fmt.Println("======= FizzBuzz here man ======")
+	for i := 0; i < 31; i++ {
+		if i%3 == 0 && i%5 == 0 {
+			fmt.Printf("Number %2d : FizzBuzz\n", i)
+		} else if i%3 == 0 {
+			fmt.Printf("Number %2d : Fizz\n", i)
+		} else if i%5 == 0 {
+			fmt.Printf("Number %2d : Buzz\n", i)
+		} else {
+			fmt.Printf("Number %2d : %d \n", i, i)
+		}
+	}
+}
+
+func LeapYearValidator() {
+	year := 2020
+
+	fmt.Printf("\n ==== Leap Year Validator ==== \n")
+	if year%400 == 0 || year%4 == 0 && year%100 != 0 { // && takes precedence over ||
+		fmt.Printf("%d is a leap year \n", year)
+	} else {
+		fmt.Printf("%d is not a leap year \n", year)
+	}
+}
+
+func Calculator() {
+	var choice int
+	var num1, num2 float64
+
+	for {
+		fmt.Println("=== Calculator ===")
+		fmt.Println("1. +")
+		fmt.Println("2. -")
+		fmt.Println("3. *")
+		fmt.Println("4. /")
+		fmt.Println("0. Exit")
+
+		fmt.Print("Enter your choice: ")
+		fmt.Scanln(&choice)
+
+		if choice == 0 {
+			fmt.Println("Exiting... bye")
+			return
+		} else {
+			switch choice {
+			case 1:
+				fmt.Print("Enter first number: ")
+				fmt.Scanln(&num1)
+
+				fmt.Print("Enter second number: ")
+				fmt.Scanln(&num2)
+
+				fmt.Printf("%.2f + %.2f = %.2f \n", num1, num2, num1+num2)
+			case 2:
+				fmt.Print("Enter first number: ")
+				fmt.Scanln(&num1)
+
+				fmt.Print("Enter second number: ")
+				fmt.Scanln(&num2)
+
+				fmt.Printf("%.2f - %.2f = %.2f \n", num1, num2, num1-num2)
+			case 3:
+				fmt.Print("Enter first number: ")
+				fmt.Scanln(&num1)
+
+				fmt.Print("Enter second number: ")
+				fmt.Scanln(&num2)
+
+				fmt.Printf("%.2f x %.2f = %.2f \n", num1, num2, num1*num2)
+			case 4:
+				fmt.Print("Enter first number: ")
+				fmt.Scanln(&num1)
+
+				fmt.Print("Enter second number: ")
+				fmt.Scanln(&num2)
+
+				if num2 == 0 {
+					fmt.Println("Division by zero is not allowed")
+				} else {
+					fmt.Printf("%.2f / %.2f = %.2f \n", num1, num2, num1/num2)
+				}
+			default:
+				fmt.Println("Invalide input, Try again")
+			}
+		}
 	}
 }
