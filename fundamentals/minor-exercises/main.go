3f96a82ed82c2dc08d0c14884709dcb918c5376a
Tonny Blair <jusanotherthursday@gmail.com>
2026-08-03 23:05:39 +0300

    some minor practice exercises to implement most concepts in go

---
+++ fundamentals/minor-exercises/main.go
@@ -0,0 +1,216 @@
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("===== Lets start to practice some Go =====")
+	user1 := Account{}
+	user1.Balance = 500
+
+	if err := user1.Deposit(-10); err != nil {
+		fmt.Println("Deposit failed:", err)
+	}
+
+	if err := user1.Withdraw(-20); err != nil {
+		fmt.Println("Withdrawal failed:", err)
+	}
+
+	book1 := Book{}
+	book1.Title = "Atomic Habits"
+	book1.Author = "James Clear"
+	book1.isAvailable = true
+
+	err := book1.Borrow()
+	if err != nil {
+		fmt.Println("Acquisition Failed:", err)
+	}
+
+	err = book1.Return()
+	if err != nil {
+		fmt.Println()
+		fmt.Println("Return Failed: ", err)
+	}
+
+	book2 := Book{}
+	book2.Title = "The Art of War"
+	book2.Author = "Sun Tzu"
+	book2.isAvailable = true
+
+	if err := book2.Borrow(); err != nil {
+		fmt.Println("Acquisition Failed:", err)
+	}
+
+	if err := book2.Return(); err != nil {
+		fmt.Println()
+		fmt.Println("Return Failed: ", err)
+	}
+
+	fmt.Printf("\n ==== Shopping Cart =====\n")
+	cart := Cart{}
+
+	phone := Product{
+		Name:     "Phone",
+		Price:    300,
+		Quantity: 2,
+	}
+	laptop := Product{
+		Name:     "Laptop",
+		Price:    500,
+		Quantity: 5,
+	}
+
+	cart.AddProduct(phone)
+	cart.AddProduct(laptop)
+	cart.ListProducts()
+	cart.RemoveProduct(phone.Name)
+}
+
+// Bank account
+type Account struct {
+	Balance float64
+}
+
+func (a *Account) Deposit(amount float64) error {
+	if amount < 0 {
+		return fmt.Errorf("deposit amount must be greater than 0")
+	}
+	a.Balance += amount
+	fmt.Printf("\nDeposit successful. New Balance: %2.f", a.Balance)
+	return nil
+}
+
+func (a *Account) Withdraw(amount float64) error {
+	if amount <= 0 {
+		return fmt.Errorf("withdrawal amount must be greater than 0")
+	}
+	if amount > a.Balance {
+		return fmt.Errorf("insufficient funds")
+	}
+	a.Balance -= amount
+	fmt.Printf("\nWithdraw successful, new balance: %.2f", a.Balance)
+	return nil
+}
+
+// Library Managment
+type Book struct {
+	Title       string
+	Author      string
+	isAvailable bool
+}
+
+func (b *Book) Borrow() error {
+	if b.isAvailable == false {
+		return fmt.Errorf("the book is currently unavailable")
+	}
+	if b.isAvailable == true {
+		fmt.Printf("You have borrowed the book %s", b.Title)
+		b.isAvailable = false
+	}
+	return nil
+}
+
+func (b *Book) Return() error {
+	if b.isAvailable == true {
+		return fmt.Errorf("you haven't borrowed this book yet.")
+	}
+	b.isAvailable = true
+	fmt.Printf("\n%s has been returned\n", b.Title)
+	return nil
+}
+
+// Shopping Cart
+type Product struct {
+	Name     string
+	Price    float64
+	Quantity int
+}
+
+type Cart struct {
+	Items []Product
+}
+
+func (c *Cart) AddProduct(product Product) error {
+	if product.Price < 0 {
+		return fmt.Errorf("price cannot be negative")
+	}
+
+	if product.Quantity <= 0 {
+		return fmt.Errorf("quantity must be greater than zero")
+	}
+
+	c.Items = append(c.Items, product)
+	return nil
+}
+
+func (c *Cart) RemoveProduct(name string) error {
+	for item := 0; item < len(c.Items); item++ {
+		if name == c.Items[item].Name {
+			c.Items = append(c.Items[:item], c.Items[item+1:]...)
+			return nil
+		}
+	}
+	return fmt.Errorf("product %s not found in cart", name)
+}
+
+func (c *Cart) CalculateTotal() float64 {
+	var total float64
+	for _, p := range c.Items {
+		total += p.Price * float64(p.Quantity)
+	}
+	return total
+}
+
+func (c *Cart) ListProducts() {
+	if len(c.Items) == 0 {
+		fmt.Println("The Cart is empty.")
+		return
+	}
+
+	fmt.Printf("\n| %-10s | %-8s | %-8s | %-8s |\n", "Name", "Quantity", "Price", "Total")
+	fmt.Println("-----------------------------------------------")
+	for _, product := range c.Items {
+		fmt.Printf("| %-10s | %-8d | %-8.2f | %-8.2f |\n", product.Name, product.Quantity, product.Price, product.Price*float64(product.Quantity))
+	}
+}
+
+// Zoo
+type Animal interface {
+	Speak()
+}
+
+type Dog struct{}
+type Cat struct{}
+type Lion struct{}
+
+func MakeThemSpeak() error {
+	return nil
+}
+
+// Payment System
+
+// To-Do App
+/*
+
+As an extra challenge, you could add features like:
+
+Prevent adding products with a negative price.
+Return an error if RemoveProduct is asked to remove something that's not in the cart.
+Print a receipt showing all products and the total.
+
+* Zoo
+Animal interface with Speak()
+Dog, Cat, Lion, etc.
+Store different animals in a single slice and make them all speak
+
+* Payment System
+Expand your current example with Stripe, PayPal, and MobileMoney
+Make some payments fail under certain conditions
+Handle the errors in Checkout
+
+* Simple CLI To-Do App
+Add tasks
+List tasks
+Mark tasks complete
+Remove tasks
+
+*/
